e013ee65439e74c8c1d0d803c853479a5735351e
Rachel Nolan <rachel.nolan@atos.net>
2015-10-17 07:53:17 +0100

    Set up scaffold and routes

---
+++ app.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var app = express();
+var path = require("path");
+
+app.use(express.static(__dirname + '/public'));
+
+
+app.get('/login', function (req, res) {
+  res.sendFile(path.join(__dirname+'/views/login.html'));
+});
+
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname+'/views/index.html'));
+});
+
+app.get('/swipe', function (req, res) {
+  res.sendFile(path.join(__dirname+'/views/swipe.html'));
+});
+
+app.get('/recipe', function (req, res) {
+  res.sendFile(path.join(__dirname+'/views/recipe.html'));
+});
+
+var server = app.listen(3000, function () {
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log('Crave Demo app listening at http://localhost/%s', port);
+});
