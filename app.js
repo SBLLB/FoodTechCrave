927dbdb8a862e6670da8776bb5f5709306b9987c
Rachel Nolan <rachel.nolan@atos.net>
2015-10-17 12:22:47 +0100

    changes port for heroku deployment

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.get('/recipe', function (req, res) {
   res.sendFile(path.join(__dirname+'/views/recipe.html'));
 });
 
-var server = app.listen(3000, function () {
+var server = app..listen((process.env.PORT || 5000), function () {
   var host = server.address().address;
   var port = server.address().port;
 
